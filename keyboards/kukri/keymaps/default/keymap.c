e9616ba7c1e98fa144f57f1dcec9a05b2c27f3e2
drhigsby <doctorhigsby@gmail.com>
2021-11-09 10:19:11 -0800

    fixing keymap

--- keyboards/kukri/keymaps/default/keymap.c
+++ keyboards/kukri/keymaps/default/keymap.c
@@ -37,15 +37,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUM] = LAYOUT(
         KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,     KC_5,            KC_6,     KC_7,    KC_8,    KC_9,     KC_0,     KC_DEL,
         KC_CAPS,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC,         KC_CIRC,  KC_AMPR, KC_ASTR, KC_MINS,  KC_EQL,   KC_PIPE,
-        xxx,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  KC_HOME,         KC_END,   xxx,     xxx,     KC_DQUO,  KC_QUOT,  xxx, xxx,
-        xxx,               xxx,     xxx,           xxx,                       xxx,            xxx,   xxx,               xxx, xxx, xxx
+        xxx,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  KC_HOME,         KC_END,   KC_PIPE, KC_BSLS, KC_DQUO,  KC_QUOT,  xxx,     xxx,
+        xxx,               xxx,     xxx,           xxx,                       xxx,            xxx,   xxx,                   xxx, xxx, xxx
   ),
 
     [_FN] = LAYOUT(
-        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,          KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-        xxx,  xxx,  xxx,  xxx,  xxx,  xxx,          xxx,  xxx,  xxx,  xxx,  xxx,  xxx, 
-        xxx,  xxx,  xxx,  xxx,  xxx,  xxx,          xxx,  xxx,  xxx,  xxx,  xxx,  xxx,  xxx,
-        xxx,              xxx,  xxx,  xxx,          xxx,  xxx,  xxx,              xxx,  xxx,  xxx       
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,        KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
+        xxx,   xxx,   xxx,   xxx,   xxx,   xxx,          xxx,   xxx,   xxx,   xxx,    xxx,    xxx, 
+        xxx,   xxx,   xxx,   xxx,   xxx,   xxx,          xxx,   xxx,   xxx,   xxx,    xxx,    xxx,  xxx,
+        xxx,              xxx,  xxx,  xxx,               xxx,   xxx,   xxx,                   xxx,  xxx,  xxx       
   )
 
 };
